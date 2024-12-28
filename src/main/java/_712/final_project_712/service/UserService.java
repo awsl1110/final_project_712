c788a63d0b0eadf42e958fa5d432944ff6ba4a83
luv'll fade code not'll <89456977+awsl1110@users.noreply.github.com>
2024-12-28 15:49:46 +0800

    创建user相关文件
    创建 user相关文件，连接数据库

---
+++ src/main/java/_712/final_project_712/service/UserService.java
@@ -0,0 +1,12 @@
+package _712.final_project_712.service;
+
+import _712.final_project_712.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    /**
+     * 获取所有用户列表
+     */
+    List<User> getAllUsers();
+} 
