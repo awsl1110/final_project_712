4cb1fadd8b34926424b5a1be30bb51dd6e4631eb
温顺涛 <89456977+awsl1110@users.noreply.github.com>
2024-12-29 16:19:20 +0800

    删除多余的验证

--- src/main/java/_712/final_project_712/controller/UserController.java
+++ src/main/java/_712/final_project_712/controller/UserController.java
@@ -73,11 +73,7 @@ public class UserController {
             
             // 修改密码
             boolean result = userService.updatePassword(userId, oldPassword, newPassword);
-            if (result) {
-                return Result.success("密码修改成功");
-            } else {
-                return Result.error("密码修改失败");
-            }
+            return Result.success("密码修改成功");
         } catch (RuntimeException e) {
             return Result.error(e.getMessage());
         }
