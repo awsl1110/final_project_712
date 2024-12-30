7101ffa53b05827fce6bbc246f4e6f59bdd13629
温顺涛 <89456977+awsl1110@users.noreply.github.com>
2024-12-30 13:34:13 +0800

    Update User.java

--- src/main/java/_712/final_project_712/model/User.java
+++ src/main/java/_712/final_project_712/model/User.java
@@ -3,7 +3,11 @@ package _712.final_project_712.model;
 import com.mybatisflex.annotation.Id;
 import com.mybatisflex.annotation.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Data
 @Table("user")
 public class User {
@@ -13,38 +17,6 @@ public class User {
     private String name;
     private String password;
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public User() {
     }
