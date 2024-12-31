45b224b02867b0d49a608d40d23b972924542a3f
awsl1110 <wenshuntao@qq.com>
2024-12-31 07:15:46 +0000

    Revert "!28 商品分类浏览"
    This reverts commit 9cf52ecacf601105568fdc655e545910b8cef2a6.

--- src/main/java/_712/final_project_712/mapper/ProductCategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package _712.final_project_712.mapper;
-
-import _712.final_project_712.model.ProductCategory;
-import com.mybatisflex.core.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ProductCategoryMapper extends BaseMapper<ProductCategory> {
-} 
--- src/main/java/_712/final_project_712/service/CategoryServiceTest.java
+++
@@ -1,5 +0,0 @@
-package _712.final_project_712.service;
-
-import _712.final_project_712.model.dto.CategoryDTO;
-import _712.final_project_712.mapper.ProductCategoryMapper;
-// ... 其他 import 保持不变 
