e2c8ff629049778cb7768df9053381885de7f02a
温顺涛 <89456977+awsl1110@users.noreply.github.com>
2025-01-03 20:16:07 +0800

    重新打包

--- src/main/resources/static/assets/address-yuD6gt1d.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./user-DuJHeInz.js";const e={getAddressList(){return s.get("/address/list")}};export{e as a};
--- src/main/resources/static/assets/product-BOdXUwmS.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./user-DuJHeInz.js";function o(){return t({url:"/product/list",method:"get"})}function u(r){return t({url:`/product/${r}`,method:"get"})}function c(){return t({url:"/product/categories",method:"get"})}function g(r){return t({url:`/product/category/${r}`,method:"get"})}export{o as a,g as b,u as c,c as g};
